4f25c868e77c71ba9e8d4921006e3bd57dfa5cd0
prisekin48 <prisekin48@gmail.com>
2020-09-26 16:00:01 +0300

    Правка координат элементов гистограммы

--- js/stat.js
+++ js/stat.js
@@ -4,11 +4,10 @@ var CLOUD_WIDTH = 420;
 var CLOUD_HEIGHT = 270;
 var CLOUD_X = 100;
 var CLOUD_Y = 10;
-var GAP = 10;
 
 var COLUMN_WIDTH = 40;
 var BASE_GAP = 50;
-var MAX_COLUMN = 130;
+var MAX_COLUMN = 150;
 
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
@@ -45,7 +44,7 @@ window.renderStatistics = function (ctx, names, times) {
 
     var columnHeight = Math.round(MAX_COLUMN * times[i - 1] / maxTime);
 
-    ctx.fillText(`${names[i - 1]}`, (CLOUD_X + GAP) * i, CLOUD_HEIGHT - GAP);
+    ctx.fillText(`${names[i - 1]}`, (CLOUD_X + BASE_GAP * 0.3) * i, CLOUD_HEIGHT - BASE_GAP * 0.05);
 
     if (names[i - 1] === 'Вы') {
       ctx.fillStyle = `rgba(255, 0, 0, 1)`;
@@ -53,13 +52,13 @@ window.renderStatistics = function (ctx, names, times) {
       ctx.fillStyle = `hsl(240, ${Math.round(Math.random() * 100)}%, 50%)`;
     }
 
-    ctx.fillRect((CLOUD_X + GAP) * i,
-        (CLOUD_Y + CLOUD_HEIGHT) - columnHeight - BASE_GAP,
+    ctx.fillRect((CLOUD_X + BASE_GAP * 0.3) * i,
+        (CLOUD_Y + CLOUD_HEIGHT) - columnHeight - BASE_GAP * 0.65,
         COLUMN_WIDTH,
         columnHeight
     );
 
     ctx.fillStyle = `#000`;
-    ctx.fillText(`${Math.round(times[i - 1])}`, (CLOUD_X + GAP) * i, CLOUD_HEIGHT - BASE_GAP - columnHeight);
+    ctx.fillText(`${Math.round(times[i - 1])}`, (CLOUD_X + BASE_GAP * 0.3) * i, CLOUD_HEIGHT - columnHeight - BASE_GAP * 0.65);
   }
 };
